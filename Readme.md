be2fd978c593b33e40df5fb09f90021de967676c
Hendrik Fick Renaldo Potgieter (Henro) <53427025+henro47@users.noreply.github.com>
2024-10-30 07:43:13 +0200

    meta: Add command args

--- Readme.md
+++ Readme.md
@@ -6,14 +6,14 @@
  - scroll to buttom of file in terminal
  - add:
     [autostart]
-    1 = python3 $HOME/path_to_repo/main.py
+    1 = python3 $HOME/path_to_repo/main.py f
  - CRTL + X to save file
 
 
 **Autostart script on startup (raspberry pi & X11 window manager) Stable:**
  - sudo nano /etc/xdg/lxsession/LXDE-pi/autostart
  - add to bottom of file:
-   @lxterminal -e python3 /home/<username>/pi-kiosk-py/main.py &
+   @lxterminal -e python3 /home/<username>/pi-kiosk-py/main.py f &
  - CRTL + X to save file
 
 
--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 import subprocess
+import sys
 
 # Replace these with your Wi-Fi credentials
 SSID = "WIFI_SSID"
 PASSWORD = "WIFI_PASSWORD"
 URL = "URL"
 
-def connect_wifi(ssid, password):
+def connect_wifi(ssid, password, browser):
     try:
         # Create a new Wi-Fi connection
         subprocess.run(["nmcli", "device", "wifi", "connect", ssid, "password", password], check=True)
         print(f"Connected to {ssid}")
-        run_chrome(URL)
+        if browser == "c":
+            # Start Chrome in kiosk mode
+            run_chrome(URL)
+        elif browser == "f":
+            # Start Firefox in kiosk mode
+            run_firefox(URL)
     except subprocess.CalledProcessError as e:
         print(f"Failed to connect: {e}")
 
-#TODO introduce command line args to switch between firefox / chromium
-#def run_chrome(url):
-#    try:
-#        # Replace 'chromium-browser' with 'chromium' if that's your executable
-#        subprocess.run(["chromium-browser", "--noerrdialogs", "--disable-session-crashed-bubble", "--disable-infobars", "--start-fullscreen", "--kiosk", url], check=True)
-#        print("chrome started")
-#    except subprocess.CalledProcessError as e:
-#        print(f"Failed to start Chromium: {e}")
+def run_chrome(url):
+    try:
+        # Replace 'chromium-browser' with 'chromium' if that's your executable
+        subprocess.run(["chromium-browser", "--noerrdialogs", "--disable-session-crashed-bubble", "--disable-infobars", "--start-fullscreen", "--kiosk", url], check=True)
+        print("chrome started")
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to start Chromium: {e}")
 
 def run_firefox(url):
     try:
@@ -31,4 +36,12 @@ def run_firefox(url):
         print(f"Failed to start firefox: {e}")
 
 if __name__ == "__main__":
-    connect_wifi(SSID, PASSWORD)
+    argumentList = sys.argv[1:]
+    if len(argumentList) == 0:
+        print("Please provide f (firefox) /c (chrome) as a command line argument")
+        sys.exit(1)
+    if str(argumentList[0]) != "f" and str(argumentList[0]) != "c":
+        print(argumentList[0])
+        print("Invalid argument. Please provide f (firefox) /c (chrome) as a command line argument")
+        sys.exit(1)
+    connect_wifi(SSID, PASSWORD, argumentList[0])
